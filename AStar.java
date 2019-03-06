f0b225e4f092f1b168f8c9fdb5edc8becb0bee3b
pk-development <paulkinselladev@gmail.com>
2019-03-06 20:50:10 +0000

    Update 1
    Bart this interface solustion will be handy for when we do AI very rather that the Human selecting best move, The idea is the create a List of Heuristics and apply each one to current state to work out the best move.

--- AStar.java
+++ AStar.java
@@ -1,4 +1,3 @@
-@@ -0,0 +1,378 @@
 package com.company;
 
 import javax.swing.*;
@@ -6,9 +5,74 @@ import java.util.*;
 
 import static javax.swing.JOptionPane.showInputDialog;
 
+interface Heuristic {
+    int calculateSum(State currentState, State goalState);
+}
+
+class ManhattanDistance implements Heuristic {
+
+    /*
+     * This search works by first finding an un-matching position from
+     * current state to goal state, we then count from that position
+     * to where it should be in the goal state. We apply this formula
+     * to every un-matching value in the array
+     *
+     *  Formula to find moves:
+     * ((linear distance from index / sqrt(len) ) +
+     * ((linear distance from index % sqrt(len) )
+     *
+     * Above formula works out shortest path to its goal position
+     */
+    public int calculateSum(State currentState, State goalState) {
+        return (currentState.getLevel() + calculateSum1(currentState, goalState) + calculateSum1(goalState, currentState));
+    }
+
+    private int calculateSum1(State currentState, State goalState) {
+        int hSum = 0;
+        int[] eState = goalState.getState();
+        int[] cState = currentState.getState();
+
+        int sqrt = (int)Math.sqrt(eState.length);
+        int arrLen = cState.length;
+
+        for(int i = 0; i < arrLen; i++) {
+
+            if(cState[i] != 0 && cState[i] != eState[i]) {
+                int distance = 0;
+                for(int j = i+1; j < arrLen; j++ ) {
+                    distance++;
+                    if(cState[i] == eState[j]) {
+                        hSum += ((distance / sqrt) + (distance % sqrt));
+                        break;
+                    }
+                }
+            }
+        }
+
+        return hSum;
+    }
+}
+
+class TilePlacement implements Heuristic {
+
+    /* Very basic Heuristic to count how many tiles
+     * are out of place
+     */
+    public int calculateSum(State currentState, State goalState) {
+        int hSum = 0;
+        int[] eState = currentState.getState();
+        int[] gState = goalState.getState();
+
+        for(int i = 0; i < eState.length; i++) {
+            if(eState[i] != gState[i]) hSum++;
+        }
+
+        return hSum + currentState.getLevel();
+    }
+}
+
 class State implements Comparable<State> {
     private int level;
-    private int sum;
     private int[] state;
 
     public State(int[] state, int level){
@@ -20,27 +84,22 @@ class State implements Comparable<State> {
         return state;
     }
 
-    public void setSum(int sum) {
-        this.sum = sum;
+    public int getLevel() {
+        return level;
     }
-
-    public int getSum() {
-        return sum;
-    }
-
     private String stateToString() {
-       int sqrt = (int)Math.sqrt(state.length);
-       StringBuilder sb = new StringBuilder("| ");
+        int sqrt = (int)Math.sqrt(state.length);
+        StringBuilder sb = new StringBuilder("| ");
 
-       for(int i = 0; i < state.length; i++) {
-           if(i < 10) sb.append(" ");
+        for(int i = 0; i < state.length; i++) {
+            if(i < 10) sb.append(" ");
 
-           sb.append(String.format("%-4s", state[i] + " | "));
+            sb.append(String.format("%-4s", state[i] + " | "));
 
-           if(i != state.length -1 && (i % sqrt) == sqrt-1) sb.append("\n| ");
-       }
+            if(i != state.length -1 && (i % sqrt) == sqrt-1) sb.append("\n| ");
+        }
 
-       return sb.toString();
+        return sb.toString();
     }
 
     public boolean equals(Object that) {
@@ -58,48 +117,24 @@ class State implements Comparable<State> {
     }
 
     @Override
+    public int hashCode() {
+        return Arrays.toString(state).hashCode();
+    }
+
+    @Override
     public int compareTo(State o) {
-        return level - o.level;
+        return Arrays.toString(state).compareTo(Arrays.toString(o.state));
     }
 }
 
 class Utils {
 
-    /* Formula to find moves:
-     * ((linear distance from index / sqrt(len) ) +
-     * ((linear distance from index % sqrt(len) )
-     */
-    public static int calculateTiles(State currentState, State endState) {
-        int hSum = 0;
-        int[] eState = endState.getState();
-        int[] state = currentState.getState();
-
-        int sqrt = (int)Math.sqrt(eState.length);
-        int arrLen = state.length;
-
-        for(int i = 0; i < arrLen; i++) {
-
-            if(state[i] != 0 && state[i] != eState[i]) {
-                int distance = 0;
-                for(int j = i+1; j < arrLen; j++ ) {
-                    distance++;
-                    if(state[i] == eState[j]) {
-                        hSum += ((distance / sqrt) + (distance % sqrt));
-                        break;
-                    }
-                }
-            }
-        }
-
-        return hSum;
-    }
-
-    public static List<State> getNextStates(State state, int level) {
+    public static Set<State> getNextStates(State state, int level) {
         int position = 0;
         int[] matrix = state.getState();
         int sqrt = (int)Math.sqrt(matrix.length);
 
-        List<State> newStates = new ArrayList<>();
+        Set<State> newStates = new TreeSet<>();
         for(int i = 0; i < matrix.length; i++) {
             if(matrix[i] == 0) {
                 position = i; //current position of Zero
@@ -113,48 +148,49 @@ class Utils {
         int west = (position % sqrt) - 1; //move left
 
 
-        if(north >= 0) {
-            int[] nArr = matrix.clone();
-            swap(nArr, position, north);
-            State northState = new State(nArr, level);
-            if(!newStates.contains(northState) && !northState.equals(state)) newStates.add(northState);
+        if(north >= 0) { //can move north
+            newStates.add(new State(swap(matrix.clone(), position, north), level));
         }
 
-        if(east < sqrt) {
-            int[] nArr = matrix.clone();
-            swap(nArr, position, position+1);
-            State eastState = new State(nArr, level);
-            if(!newStates.contains(eastState) && !eastState.equals(state)) newStates.add(eastState);
+        if(east < sqrt) { //can move east
+            newStates.add(new State(swap(matrix.clone(), position, position+1), level));
         }
 
-        if(west >= 0) {
-            int[] nArr = matrix.clone();
-            swap(nArr, position, position-1);
-            State westState = new State(nArr, level);
-            if(!newStates.contains(westState) && !westState.equals(state)) newStates.add(westState);
+        if(west >= 0) { //can move west
+            newStates.add(new State(swap(matrix.clone(), position, position-1), level));
         }
 
-        if(south < matrix.length) {
-            int[] nArr = matrix.clone();
-            swap(nArr, position, south);
-            State southState = new State(nArr, level);
-            if(!newStates.contains(southState) && !southState.equals(state)) newStates.add(southState);
+        if(south < matrix.length) { //can move south
+            newStates.add(new State(swap(matrix.clone(), position, south), level));
         }
 
         return newStates;
     }
 
-    public static void swap(int[] arr, int i, int j) {
+    public static int calculateBestMove(List<Heuristic> heuristics, State currentState, State goalState) {
+        int best = -1;
+
+        for(Heuristic h : heuristics) {
+            int sum = h.calculateSum(currentState, goalState);
+            if(sum > best) best = sum;
+        }
+
+        return best;
+    }
+
+    public static int[] swap(int[] arr, int i, int j) {
         int tmp = arr[i];
         arr[i] = arr[j];
         arr[j] = tmp;
+
+        return arr;
     }
 }
 
 public class Main {
 
     public static void main(String[] args) {
-	    String startState = showInputDialog(null,
+        String startState = showInputDialog(null,
                 "Enter initial state", "0 1 3 4 5 2 6 8 9 10 7 11 13 14 15 12");
         String endState = showInputDialog(null,
                 "Enter end state", "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 0");
@@ -164,9 +200,11 @@ public class Main {
                 && startState.replaceAll("\\d+", "").trim().equals("")
                 && endState.replaceAll("\\d+", "").trim().equals("")) {
 
-            //clean up spaces
+            //clean up spaces and spilt elements
             String[] startStateArr = startState.replaceAll("\\s+", " ").trim().split(" ");
             String[] endStateArr = endState.replaceAll("\\s+", " ").trim().split(" ");
+
+            //Make sure user entered a valid order array
             Set<String> sortedStartSet = new TreeSet<>(Arrays.asList(startStateArr));
             Set<String> sortedEndSet = new TreeSet<>(Arrays.asList(endStateArr));
             Set<String> testSet = new TreeSet<>();
@@ -178,9 +216,11 @@ public class Main {
             boolean isEndSquare = Math.sqrt(eLen) % 1 == 0.0;
 
             if(!(isStartSquare || isEndSquare)) {
-                JOptionPane.showMessageDialog(null, "Have to be an N x N matrix\n\nExample: 3 x 3\n\n= 0 1 2 3 4 5 6 7 8");
+                JOptionPane.showMessageDialog(null,
+                        "Have to be an N x N matrix\n\nExample: 3 x 3\n\n= 0 1 2 3 4 5 6 7 8");
             } else if(sLen != eLen) {
-                JOptionPane.showMessageDialog(null, "Matrix sizes don't match A=" + sLen + " B=" + eLen);
+                JOptionPane.showMessageDialog(null,
+                        "Matrix sizes don't match A=" + sLen + " B=" + eLen);
             } else {
 
                 /* Build an ordered set of values from 0 to n
@@ -195,13 +235,15 @@ public class Main {
                 if(sortedStartSet.equals(testSet) && sortedEndSet.equals(testSet)){
                     findEndState(startStateArr, endStateArr);
                 } else {
-                    JOptionPane.showMessageDialog(null, "Matrix is not valid must be like:\n" + testSet +
-                    "\n\nYours are:\n" + sortedStartSet + "\n" + sortedEndSet);
+                    JOptionPane.showMessageDialog(null,
+                            "Matrix is not valid must be like:\n" + testSet +
+                            "\n\nYours are:\n" + sortedStartSet + "\n" + sortedEndSet);
                 }
             }
 
         } else System.out.println("Invalid Matrix");
     }
+
 
     public static void findEndState(String[] start, String[] end) {
         int[] startState = new int[start.length];
@@ -216,18 +258,26 @@ public class Main {
         State startNode = new State(startState, 0);
         State endNode = new State(endState, 0);
 
-        List<State> open = new ArrayList<>();
-        List<State> closed = new ArrayList<>();
+        Set<State> open = new TreeSet<>();
+        Set<State> closed = new LinkedHashSet<>(); //keep order of insertion
 
         int level = 1;
         boolean noGoal = true;
-        System.out.println("Choose next state");
-
-        for(State op : Utils.getNextStates(startNode,level)) {
-            if(!open.contains(op)) open.add(op);
-        }
-
+        open.addAll(Utils.getNextStates(startNode,level));
         State selectedNode = startNode;
+
+        /*
+         * Bart here is an example of just 2 types of heuristics
+         * ManhattanDistance is more accurate then TilePlacement
+         * and the idea behind creating these is to put all the heuristics
+         * int a List so we can loop through the applying each to
+         * the current state to work out best route to take.
+         *
+         * Try come up with more - These are here just as an example
+         */
+        Heuristic h1 = new ManhattanDistance();
+        Heuristic h2 = new TilePlacement();
+
         while (noGoal) {
             closed.add(selectedNode);// we selected this
 
@@ -236,16 +286,20 @@ public class Main {
                 System.out.println("Your win");
 
                 for(State cl : closed) {
-                    System.out.println("*****************\n" + cl + "\n");
+                    System.out.printf("******* Level(%d) ********\n%s\n", cl.getLevel(), cl);
                 }
+
             } else {
                 JOptionPane.showMessageDialog(null,
-                        "Your State\n" + selectedNode + "\nEnd State\n" + endNode);
+                        "Your State\n" + selectedNode
+                                + "\nEnd State\n" + endNode);
+
+                System.out.println("Choose next state");
 
                 int index =1;
                 for(State s : open) {
-                    System.out.println("\n---------------(LEVEL " + level + ")----------------------\n" +
-                            index++ + ")\n" +s + "\nh=" + (Utils.calculateTiles(s, endNode) + Utils.calculateTiles(endNode, s)));
+                    System.out.println("---------------(LEVEL " + level + ")----------------------\n" +
+                            index++ + ")\n" +s + "\nh + g = " + h2.calculateSum(s, endNode));
                 }
 
                 Object[] oStates = open.toArray();
@@ -253,127 +307,19 @@ public class Main {
                         "Choose next state",
                         "Title", 0, null, oStates, oStates[0]);
 
+                /* We need to keep all these states even though I delete here because
+                 * in Tut for AI if we come to a state with only one move we need to go
+                 * back a level and add that to the closed state then pick next best
+                 * state from open
+                 */
                 open.clear(); //delete other states
                 for(State op : Utils.getNextStates(selectedNode,level)) {
-                    if(!closed.contains(op)) open.add(op); //did we already choose this
+                    if(!closed.contains(op)) open.add(op); //only add states not used
                 }
 
                 level++;
             }
-
-
         }
 
     }
 }
-
-/*
- start: "2 4 3 1 0 6 7 5 8"
- end:   "1 2 3 4 5 6 7 8 0"
-Your win
-*****************
-| 2 | 4 | 3 |
-| 1 | 0 | 6 |
-| 7 | 5 | 8 |
-
-*****************
-| 2 | 4 | 3 |
-| 1 | 5 | 6 |
-| 7 | 0 | 8 |
-
-*****************
-| 2 | 4 | 3 |
-| 1 | 5 | 6 |
-| 7 | 8 | 0 |
-
-*****************
-| 2 | 4 | 3 |
-| 1 | 5 | 0 |
-| 7 | 8 | 6 |
-
-*****************
-| 2 | 4 | 3 |
-| 1 | 0 | 5 |
-| 7 | 8 | 6 |
-
-*****************
-| 2 | 0 | 3 |
-| 1 | 4 | 5 |
-| 7 | 8 | 6 |
-
-*****************
-| 0 | 2 | 3 |
-| 1 | 4 | 5 |
-| 7 | 8 | 6 |
-
-*****************
-| 1 | 2 | 3 |
-| 0 | 4 | 5 |
-| 7 | 8 | 6 |
-
-*****************
-| 1 | 2 | 3 |
-| 4 | 0 | 5 |
-| 7 | 8 | 6 |
-
-*****************
-| 1 | 2 | 3 |
-| 4 | 5 | 0 |
-| 7 | 8 | 6 |
-
-*****************
-| 1 | 2 | 3 |
-| 4 | 5 | 6 |
-| 7 | 8 | 0 |
-
-
-
-15 x 15
-start: "0 1 3 4 5 2 6 8 9 10 7 11 13 14 15 12"
-  end: "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 0"
-
-Your win
-*****************
-| 0 | 1 | 3 | 4 |
-| 5 | 2 | 6 | 8 |
-| 9 | 10 | 7 | 11 |
-| 13 | 14 | 15 | 12 |
-
-*****************
-| 1 | 0 | 3 | 4 |
-| 5 | 2 | 6 | 8 |
-| 9 | 10 | 7 | 11 |
-| 13 | 14 | 15 | 12 |
-
-*****************
-| 1 | 2 | 3 | 4 |
-| 5 | 0 | 6 | 8 |
-| 9 | 10 | 7 | 11 |
-| 13 | 14 | 15 | 12 |
-
-*****************
-| 1 | 2 | 3 | 4 |
-| 5 | 6 | 0 | 8 |
-| 9 | 10 | 7 | 11 |
-| 13 | 14 | 15 | 12 |
-
-*****************
-| 1 | 2 | 3 | 4 |
-| 5 | 6 | 7 | 8 |
-| 9 | 10 | 0 | 11 |
-| 13 | 14 | 15 | 12 |
-
-*****************
-| 1 | 2 | 3 | 4 |
-| 5 | 6 | 7 | 8 |
-| 9 | 10 | 11 | 0 |
-| 13 | 14 | 15 | 12 |
-
-*****************
-| 1 | 2 | 3 | 4 |
-| 5 | 6 | 7 | 8 |
-| 9 | 10 | 11 | 12 |
-| 13 | 14 | 15 | 0 |
-
-
- */
